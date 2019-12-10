1be1d18eafcfb243d5b9a847116827cb4af76fdc
Supinic <supinic@protonmail.com>
2019-12-10 19:34:05 +0100

    #suggestion 2186 Added supibot banphrases checking to $bancheck

--- commands/bancheck.sql
+++ commands/bancheck.sql
@@ -49,34 +49,51 @@ VALUES
 
 	const message = rest.join(\" \");
 	const targetChannel = sb.Channel.get(channel.replace(/^#/, \"\"));
-
-	if (!targetChannel || !targetChannel.Banphrase_API_Type || !targetChannel.Banphrase_API_URL) {
-		return { reply: \"Target channel has no banphrase API configured!\" };
+	if (!targetChannel) {
+		return {
+			reply: \"Invalid channel provided!\"
+		};
 	}
 	else if (context.channel?.ID === targetChannel.ID)  {
-		return { reply: \"Don\'t you think it\'s a bit silly to bancheck the channel you\'re in? PepeLaugh\" };
+		return {
+			reply: \"Don\'t you think it\'s a bit silly to bancheck the channel you\'re in? PepeLaugh\"
+		};
 	}
 
 	if (!targetChannel.Links_Allowed) {
 		const linkCheck = message.replace(sb.Config.get(\"LINK_REGEX\"), \"\");
 		if (linkCheck !== message) {
-			return { reply: \"Links are not allowed in that channel, so it would probably get timed out.\" };
+			return {
+				reply: \"Links are not allowed in that channel, so your message would probably get timed out.\"
+			};
 		}
 	}
 
-	const check = await sb.Banphrase.executeExternalAPI(
-		message, 
-		targetChannel.Banphrase_API_Type,
-		targetChannel.Banphrase_API_URL
-	);
+	if (targetChannel.Banphrase_API_Type === \"Pajbot\") {
+		const check = await sb.Banphrase.executeExternalAPI(
+			message,
+			targetChannel.Banphrase_API_Type,
+			targetChannel.Banphrase_API_URL
+		);
 
-	if (!check) {
-		return { reply: \"That message will not get timed out by the banphrase API.\" };
+		if (check) {
+			console.log(\"bancheck result\", check);
+			return {
+				reply: `The phrase \"${check}\" will get you timed out!`
+			};
+		}
+	}
+
+	const { string } = await sb.Banphrase.execute(message, targetChannel);
+	if (message === string) {
+		return {
+			reply: \"That message should be fine.\"
+		};
 	}
 	else {
-		// console.log(context.user.Name + \" checked message \'\" + message + \"\' for channel \" + targetChannel.Name);
-		console.log(\"bancheck result\", check);
-		return { reply: \"The phrase \\\"\" + check + \"\\\" will get you timed out!\" };
+		return {
+			reply: \"That message is most likely going to get timed out, based on my banphrases.\"
+		};
 	}
 })',
 		'Argument 1: A Twitch channel, prefixed with \"#\". That channel must have the banphrase API set up.
@@ -98,33 +115,50 @@ ON DUPLICATE KEY UPDATE
 
 	const message = rest.join(\" \");
 	const targetChannel = sb.Channel.get(channel.replace(/^#/, \"\"));
-
-	if (!targetChannel || !targetChannel.Banphrase_API_Type || !targetChannel.Banphrase_API_URL) {
-		return { reply: \"Target channel has no banphrase API configured!\" };
+	if (!targetChannel) {
+		return {
+			reply: \"Invalid channel provided!\"
+		};
 	}
 	else if (context.channel?.ID === targetChannel.ID)  {
-		return { reply: \"Don\'t you think it\'s a bit silly to bancheck the channel you\'re in? PepeLaugh\" };
+		return {
+			reply: \"Don\'t you think it\'s a bit silly to bancheck the channel you\'re in? PepeLaugh\"
+		};
 	}
 
 	if (!targetChannel.Links_Allowed) {
 		const linkCheck = message.replace(sb.Config.get(\"LINK_REGEX\"), \"\");
 		if (linkCheck !== message) {
-			return { reply: \"Links are not allowed in that channel, so it would probably get timed out.\" };
+			return {
+				reply: \"Links are not allowed in that channel, so your message would probably get timed out.\"
+			};
 		}
 	}
 
-	const check = await sb.Banphrase.executeExternalAPI(
-		message, 
-		targetChannel.Banphrase_API_Type,
-		targetChannel.Banphrase_API_URL
-	);
+	if (targetChannel.Banphrase_API_Type === \"Pajbot\") {
+		const check = await sb.Banphrase.executeExternalAPI(
+			message,
+			targetChannel.Banphrase_API_Type,
+			targetChannel.Banphrase_API_URL
+		);
 
-	if (!check) {
-		return { reply: \"That message will not get timed out by the banphrase API.\" };
+		if (check) {
+			console.log(\"bancheck result\", check);
+			return {
+				reply: `The phrase \"${check}\" will get you timed out!`
+			};
+		}
+	}
+
+	const { string } = await sb.Banphrase.execute(message, targetChannel);
+	if (message === string) {
+		return {
+			reply: \"That message should be fine.\"
+		};
 	}
 	else {
-		// console.log(context.user.Name + \" checked message \'\" + message + \"\' for channel \" + targetChannel.Name);
-		console.log(\"bancheck result\", check);
-		return { reply: \"The phrase \\\"\" + check + \"\\\" will get you timed out!\" };
+		return {
+			reply: \"That message is most likely going to get timed out, based on my banphrases.\"
+		};
 	}
 })'
