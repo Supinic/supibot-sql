5867bdcf437e494764a858d4740e52bec7bccca4
Supinic <supinic@protonmail.com>
2019-12-18 18:41:57 +0100

    #suggestion 2281 #refactor Added commas to be used along with periods for floats
    also, refactored command to new definition

--- commands/currency.sql
+++ commands/currency.sql
@@ -39,21 +39,42 @@ VALUES
 		1,
 		1,
 		0,
-		'async (extra, amount, first, separator, second) => {
+		'(async function currency (context, amount, first, separator, second)  {
 	if (!second) {
 		second = separator;
-		separator = first;
 		first = amount;
 		amount = \"1\";
 	}
 
 	if (!first || !second) {
-		return { reply: \"Both currencies must be provided!\", meta: { skipCooldown: true } };
+		return {
+			reply: \"Both currencies must be provided!\",
+			meta: { skipCooldown: true }
+		};
 	}
 
-	amount = amount.replace(/k/gi, \"0\".repeat(3)).replace(/m/gi, \"0\".repeat(6)).replace(/b/gi, \"0\".repeat(9));
+	let multiplier = 1;
+	if (/k/i.test(amount)) {
+		multiplier = 1.0e3;
+	}
+	else if (/m/i.test(amount)) {
+		multiplier = 1.0e6;
+	}
+	else if (/b/i.test(amount)) {
+		multiplier = 1.0e9;
+	}
+	else if (/t/i.test(amount)) {
+		multiplier = 1.0e12;
+	}
+
+	amount = amount.replace(/[kmbt]/gi, \"\").replace(/,/g, \".\");
 	if (!Number(amount)) {
-		return { reply: \"The amount must be a finite number!\", meta: { skipCooldown: true } };
+		console.log(amount);
+
+		return {
+			reply: \"The amount must be a finite number!\",
+			meta: { skipCooldown: true }
+		};
 	}
 
 	const currencySymbol = first.toUpperCase() + \"_\" + second.toUpperCase();
@@ -61,24 +82,23 @@ VALUES
 		return { reply: \"Both currencies must be represented by exactly 3 letters!\" };
 	}
 
-	const url = [
-		\"https://free.currencyconverterapi.com/api/v6/convert\",
-		\"?compact=ultra\",
-		\"&q=\" + currencySymbol,
-		\"&apiKey=\" + sb.Config.get(\"API_FREE_CURRENCY_CONVERTER\")
-	].join(\"\");
+	const params = new sb.URLParams()
+		.set(\"compact\", \"ultra\")
+		.set(\"q\", currencySymbol)
+		.set(\"apiKey\", sb.Config.get(\"API_FREE_CURRENCY_CONVERTER\"));
 
 	let rawData = null;
 	let ratio = null;
 	try {
-		rawData = await sb.Utils.request(url);
+		rawData = await sb.Utils.request({
+			uri: \"https://free.currencyconverterapi.com/api/v6/convert?\" + params.toString()
+		});
+
 		ratio = JSON.parse(rawData)[currencySymbol];
 	}
 	catch (e) {
-		console.error(\"Currency convert error\", e, rawData);
-
 		if (e.error) {
-			return { 
+			return {
 				reply: \"API returned error: \" + sb.Utils.removeHTML(e.error).replace(/\\s+/g, \" \")
 			};
 		}
@@ -89,13 +109,15 @@ VALUES
 
 	if (typeof ratio === \"number\") {
 		return {
-			reply: amount + \" \" + first + \" = \" + sb.Utils.round(amount * ratio, 3) + \" \" + second
+			reply: `${amount * multiplier} ${first} = ${sb.Utils.round(amount * multiplier * ratio, 3)} ${second}`
 		};
 	}
 	else {
-		return { reply: \"One or both currencies were not recognized\" };
+		return {
+			reply: \"One or both currencies were not recognized\"
+		};
 	}
-}',
+})',
 		'$currency <currency code> to <currency code> => Converts 1 of currency to the other
 $currency <amount> <currency code> to <currency code> => Converts that amount of currency to the other
 
@@ -105,21 +127,42 @@ $currency 5000 SEK to AUD',
 	)
 
 ON DUPLICATE KEY UPDATE
-	Code = 'async (extra, amount, first, separator, second) => {
+	Code = '(async function currency (context, amount, first, separator, second)  {
 	if (!second) {
 		second = separator;
-		separator = first;
 		first = amount;
 		amount = \"1\";
 	}
 
 	if (!first || !second) {
-		return { reply: \"Both currencies must be provided!\", meta: { skipCooldown: true } };
+		return {
+			reply: \"Both currencies must be provided!\",
+			meta: { skipCooldown: true }
+		};
 	}
 
-	amount = amount.replace(/k/gi, \"0\".repeat(3)).replace(/m/gi, \"0\".repeat(6)).replace(/b/gi, \"0\".repeat(9));
+	let multiplier = 1;
+	if (/k/i.test(amount)) {
+		multiplier = 1.0e3;
+	}
+	else if (/m/i.test(amount)) {
+		multiplier = 1.0e6;
+	}
+	else if (/b/i.test(amount)) {
+		multiplier = 1.0e9;
+	}
+	else if (/t/i.test(amount)) {
+		multiplier = 1.0e12;
+	}
+
+	amount = amount.replace(/[kmbt]/gi, \"\").replace(/,/g, \".\");
 	if (!Number(amount)) {
-		return { reply: \"The amount must be a finite number!\", meta: { skipCooldown: true } };
+		console.log(amount);
+
+		return {
+			reply: \"The amount must be a finite number!\",
+			meta: { skipCooldown: true }
+		};
 	}
 
 	const currencySymbol = first.toUpperCase() + \"_\" + second.toUpperCase();
@@ -127,24 +170,23 @@ ON DUPLICATE KEY UPDATE
 		return { reply: \"Both currencies must be represented by exactly 3 letters!\" };
 	}
 
-	const url = [
-		\"https://free.currencyconverterapi.com/api/v6/convert\",
-		\"?compact=ultra\",
-		\"&q=\" + currencySymbol,
-		\"&apiKey=\" + sb.Config.get(\"API_FREE_CURRENCY_CONVERTER\")
-	].join(\"\");
+	const params = new sb.URLParams()
+		.set(\"compact\", \"ultra\")
+		.set(\"q\", currencySymbol)
+		.set(\"apiKey\", sb.Config.get(\"API_FREE_CURRENCY_CONVERTER\"));
 
 	let rawData = null;
 	let ratio = null;
 	try {
-		rawData = await sb.Utils.request(url);
+		rawData = await sb.Utils.request({
+			uri: \"https://free.currencyconverterapi.com/api/v6/convert?\" + params.toString()
+		});
+
 		ratio = JSON.parse(rawData)[currencySymbol];
 	}
 	catch (e) {
-		console.error(\"Currency convert error\", e, rawData);
-
 		if (e.error) {
-			return { 
+			return {
 				reply: \"API returned error: \" + sb.Utils.removeHTML(e.error).replace(/\\s+/g, \" \")
 			};
 		}
@@ -155,10 +197,12 @@ ON DUPLICATE KEY UPDATE
 
 	if (typeof ratio === \"number\") {
 		return {
-			reply: amount + \" \" + first + \" = \" + sb.Utils.round(amount * ratio, 3) + \" \" + second
+			reply: `${amount * multiplier} ${first} = ${sb.Utils.round(amount * multiplier * ratio, 3)} ${second}`
 		};
 	}
 	else {
-		return { reply: \"One or both currencies were not recognized\" };
+		return {
+			reply: \"One or both currencies were not recognized\"
+		};
 	}
-}'
+})'
