bacb4b84bba24d67755e81705639c2b8cf8f3327
Supinic <supinic@protonmail.com>
2019-12-20 17:34:36 +0100

    #refactor now works with any subreddit
    also, added 18+ checks for subreddits and channels.

--- commands/randommeme.sql
+++ commands/randommeme.sql
@@ -39,52 +39,128 @@ VALUES
 		1,
 		1,
 		0,
-		'async (extra, ...subreddits) => {
-	let target = [\"dankmemes\", \"memes\", \"pewdiepiesubmissions\",  \"okbuddyretard\"];
-	if (subreddits.length > 0) {
-		const allowedSubreddits = sb.Config.get(\"RANDOM_MEME_SUBREDDITS\");
-		if (subreddits.some(i => !allowedSubreddits.includes(i))) {
-			return {
-				reply: \"The following subreddit(s) are not available on Twitch: \" + subreddits.filter(i => !allowedSubreddits.includes(i))
-			};
-		}
-		target = subreddits;
+		'(async function randomMeme (context, subreddit) {
+	if (!subreddit) {
+		subreddit = sb.Utils.randArray([\"dankmemes\", \"memes\", \"pewdiepiesubmissions\"]);
 	}
 
-	const url = \"https://meme-api.herokuapp.com/gimme/\" + target.join(\"+\");
-	try {
-		const data = JSON.parse(await sb.Utils.request(url));
-		return { reply: data.title + \": \" + data.url };
+	subreddit = subreddit.toLowerCase();
+
+	const safeSpace = Boolean(!context.privateMessage && !context.channel.NSFW);
+	const check = JSON.parse(await sb.Utils.request({
+		uri: `https://www.reddit.com/r/${subreddit}/about.json`,
+		headers: {
+			\"Cookie\": \"_options={%22pref_quarantine_optin%22:true};\"
+		}
+	}));
+
+	if (!check) {
+		return {
+			reply: \"No data obtained...\"
+		};
 	}
-	catch (e) {
-		console.warn(\"random meme\", e);
-		return  { reply: \"API not available!\" };
+	else if (check.error === 403) {
+		return {
+			reply: \"That subreddit is \" + check.reason + \"!\"
+		};
 	}
-}',
+	else if (!check.data || (Array.isArray(check.data.children) && check.data.children.length === 0)) {
+		return {
+			reply: \"That subreddit does not exist!\"
+		};
+	}
+	else if (check.data.over18 && safeSpace) {
+		return {
+			reply: \"That subreddit is flagged as 18+, and thus not safe to post here!\"
+		};
+	}
+
+	const posts = JSON.parse(await sb.Utils.request({
+		uri: `https://www.reddit.com/r/${subreddit}/hot.json`,	
+		headers: {
+			\"Cookie\": \"_options={%22pref_quarantine_optin%22:true};\"
+		}
+	}));
+
+	const children = (safeSpace)
+		? posts.data.children.filter(i => !i.over_18)
+		: posts.data.children;
+	
+	const quarantine = (check.data.quarantine) ? \"⚠\" : \"\";
+	const post = sb.Utils.randArray(children);
+	if (!post) {
+		return {
+			reply: \"No suitable posts found!\"
+		}
+	}
+	else {
+		return {
+			reply: `${quarantine} ${post.data.title} ${post.data.url}`
+		}
+	}
+})',
 		NULL,
 		NULL
 	)
 
 ON DUPLICATE KEY UPDATE
-	Code = 'async (extra, ...subreddits) => {
-	let target = [\"dankmemes\", \"memes\", \"pewdiepiesubmissions\",  \"okbuddyretard\"];
-	if (subreddits.length > 0) {
-		const allowedSubreddits = sb.Config.get(\"RANDOM_MEME_SUBREDDITS\");
-		if (subreddits.some(i => !allowedSubreddits.includes(i))) {
-			return {
-				reply: \"The following subreddit(s) are not available on Twitch: \" + subreddits.filter(i => !allowedSubreddits.includes(i))
-			};
-		}
-		target = subreddits;
+	Code = '(async function randomMeme (context, subreddit) {
+	if (!subreddit) {
+		subreddit = sb.Utils.randArray([\"dankmemes\", \"memes\", \"pewdiepiesubmissions\"]);
 	}
 
-	const url = \"https://meme-api.herokuapp.com/gimme/\" + target.join(\"+\");
-	try {
-		const data = JSON.parse(await sb.Utils.request(url));
-		return { reply: data.title + \": \" + data.url };
+	subreddit = subreddit.toLowerCase();
+
+	const safeSpace = Boolean(!context.privateMessage && !context.channel.NSFW);
+	const check = JSON.parse(await sb.Utils.request({
+		uri: `https://www.reddit.com/r/${subreddit}/about.json`,
+		headers: {
+			\"Cookie\": \"_options={%22pref_quarantine_optin%22:true};\"
+		}
+	}));
+
+	if (!check) {
+		return {
+			reply: \"No data obtained...\"
+		};
 	}
-	catch (e) {
-		console.warn(\"random meme\", e);
-		return  { reply: \"API not available!\" };
+	else if (check.error === 403) {
+		return {
+			reply: \"That subreddit is \" + check.reason + \"!\"
+		};
 	}
-}'
+	else if (!check.data || (Array.isArray(check.data.children) && check.data.children.length === 0)) {
+		return {
+			reply: \"That subreddit does not exist!\"
+		};
+	}
+	else if (check.data.over18 && safeSpace) {
+		return {
+			reply: \"That subreddit is flagged as 18+, and thus not safe to post here!\"
+		};
+	}
+
+	const posts = JSON.parse(await sb.Utils.request({
+		uri: `https://www.reddit.com/r/${subreddit}/hot.json`,	
+		headers: {
+			\"Cookie\": \"_options={%22pref_quarantine_optin%22:true};\"
+		}
+	}));
+
+	const children = (safeSpace)
+		? posts.data.children.filter(i => !i.over_18)
+		: posts.data.children;
+	
+	const quarantine = (check.data.quarantine) ? \"⚠\" : \"\";
+	const post = sb.Utils.randArray(children);
+	if (!post) {
+		return {
+			reply: \"No suitable posts found!\"
+		}
+	}
+	else {
+		return {
+			reply: `${quarantine} ${post.data.title} ${post.data.url}`
+		}
+	}
+})'
