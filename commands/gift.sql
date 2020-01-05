dd0b69a9a1aa17d9b73f91ffd4b655a94018c13a
Supinic <supinic@protonmail.com>
2020-01-05 18:52:22 +0100

    #fix no longer allows gifting cookies to people who never ate one

--- commands/gift.sql
+++ commands/gift.sql
@@ -39,7 +39,7 @@ VALUES
 		1,
 		1,
 		0,
-		'async (extra, type, target) => {
+		'(async function gift (context, type, target) {
 	if (!type) {
 		return { reply: \"No type provided!\" };
 	}
@@ -58,52 +58,62 @@ VALUES
 				return { reply: \"I appreciate the gesture, but thanks, I don\'t eat sweets :)\" };
 			}
 
-			const check = (await sb.Query.getRecordset(rs => rs
+			const sourceUserCheck = await sb.Query.getRecordset(rs => rs
 				.select(\"Cookie_Today\", \"Cookie_Is_Gifted\")
 				.from(\"chat_data\", \"Extra_User_Data\")
-				.where(\"User_Alias = %n\", extra.user.ID)
-			))[0];
+				.where(\"User_Alias = %n\", context.user.ID)
+				.single()
+			);
 
-			if (!check) {
+			if (!sourceUserCheck) {
 				await sb.Query.raw([
-					\"INSERT INTO chat_data.Extra_User_Data (User_Alias)\", 
-					\"VALUES (\" + extra.user.ID + \")\"
+					\"INSERT INTO chat_data.Extra_User_Data (User_Alias)\",
+					\"VALUES (\" + context.user.ID + \")\"
 				].join(\" \"));
 			}
-			else if (check.Cookie_Today) {
+			else if (sourceUserCheck.Cookie_Today) {
 				return { reply: \"You already ate or gifted away your cookie today, so you can\'t gift it to someone else!\" };
 			}
-			else if (check.Cookie_Is_Gifted) {
+			else if (sourceUserCheck.Cookie_Is_Gifted) {
 				return { reply: \"That cookie was gifted to you! Eat it, don\'t give it away!\" };
 			}
 
-			const targetCheck = (await sb.Query.getRecordset(rs => rs
+			const targetUserCheck = await sb.Query.getRecordset(rs => rs
 				.select(\"Cookie_Today\")
 				.from(\"chat_data\", \"Extra_User_Data\")
 				.where(\"User_Alias = %n\", targetUser.ID)
-			))[0];
-			if (targetCheck && !targetCheck.Cookie_Today) {
-				return { reply: \"That user hasn\'t eaten their cookie today, so you would be wasting your gift! Get them to eat it!\" };
+				.single()
+			);
+
+			if (!targetUserCheck) {
+				return {
+					reply: \"That user has never eaten a cookie before, so gifting them one is kinda pointless!\"
+				};
+			}
+			else if (!targetUserCheck.Cookie_Today) {
+				return {
+					reply: \"That user hasn\'t eaten their cookie today, so you would be wasting your gift! Get them to eat it!\"
+				};
 			}
 
 			await sb.Query.raw([
-				\"INSERT INTO chat_data.Extra_User_Data (User_Alias, Cookie_Today,  Cookie_Gifts_Sent)\", 
-				\"VALUES (\" + extra.user.ID + \", 1, 1)\",
+				\"INSERT INTO chat_data.Extra_User_Data (User_Alias, Cookie_Today,  Cookie_Gifts_Sent)\",
+				\"VALUES (\" + context.user.ID + \", 1, 1)\",
 				\"ON DUPLICATE KEY UPDATE Cookie_Today = 1, Cookie_Gifts_Sent = Cookie_Gifts_Sent + 1\"
 			].join(\" \"));
 
 			await sb.Query.raw([
-				\"UPDATE chat_data.Extra_User_Data\", 
+				\"UPDATE chat_data.Extra_User_Data\",
 				\"SET Cookie_Today = 0, Cookie_Is_Gifted = 1, Cookie_Gifts_Received = Cookie_Gifts_Received + 1\",
 				\"WHERE User_Alias = \" + targetUser.ID
 			].join(\" \"));
-			
-			return { reply: \"Successfully given your cookie for today to \" + targetUser.Name + \" :)\" };	
+
+			return { reply: \"Successfully given your cookie for today to \" + targetUser.Name + \" :)\" };
 		}
 
 		default: return { reply: \"Target type cannot be gifted (yet, at least).\" };
 	}
-}',
+})',
 		'Gifts another user some item. Depends on the type chosen.
 
 $gift cookie <user> => Gifts your daily cookie to that user. They must have eaten their own for today first.',
@@ -111,7 +121,7 @@ $gift cookie <user> => Gifts your daily cookie to that user. They must have eate
 	)
 
 ON DUPLICATE KEY UPDATE
-	Code = 'async (extra, type, target) => {
+	Code = '(async function gift (context, type, target) {
 	if (!type) {
 		return { reply: \"No type provided!\" };
 	}
@@ -130,49 +140,59 @@ ON DUPLICATE KEY UPDATE
 				return { reply: \"I appreciate the gesture, but thanks, I don\'t eat sweets :)\" };
 			}
 
-			const check = (await sb.Query.getRecordset(rs => rs
+			const sourceUserCheck = await sb.Query.getRecordset(rs => rs
 				.select(\"Cookie_Today\", \"Cookie_Is_Gifted\")
 				.from(\"chat_data\", \"Extra_User_Data\")
-				.where(\"User_Alias = %n\", extra.user.ID)
-			))[0];
+				.where(\"User_Alias = %n\", context.user.ID)
+				.single()
+			);
 
-			if (!check) {
+			if (!sourceUserCheck) {
 				await sb.Query.raw([
-					\"INSERT INTO chat_data.Extra_User_Data (User_Alias)\", 
-					\"VALUES (\" + extra.user.ID + \")\"
+					\"INSERT INTO chat_data.Extra_User_Data (User_Alias)\",
+					\"VALUES (\" + context.user.ID + \")\"
 				].join(\" \"));
 			}
-			else if (check.Cookie_Today) {
+			else if (sourceUserCheck.Cookie_Today) {
 				return { reply: \"You already ate or gifted away your cookie today, so you can\'t gift it to someone else!\" };
 			}
-			else if (check.Cookie_Is_Gifted) {
+			else if (sourceUserCheck.Cookie_Is_Gifted) {
 				return { reply: \"That cookie was gifted to you! Eat it, don\'t give it away!\" };
 			}
 
-			const targetCheck = (await sb.Query.getRecordset(rs => rs
+			const targetUserCheck = await sb.Query.getRecordset(rs => rs
 				.select(\"Cookie_Today\")
 				.from(\"chat_data\", \"Extra_User_Data\")
 				.where(\"User_Alias = %n\", targetUser.ID)
-			))[0];
-			if (targetCheck && !targetCheck.Cookie_Today) {
-				return { reply: \"That user hasn\'t eaten their cookie today, so you would be wasting your gift! Get them to eat it!\" };
+				.single()
+			);
+
+			if (!targetUserCheck) {
+				return {
+					reply: \"That user has never eaten a cookie before, so gifting them one is kinda pointless!\"
+				};
+			}
+			else if (!targetUserCheck.Cookie_Today) {
+				return {
+					reply: \"That user hasn\'t eaten their cookie today, so you would be wasting your gift! Get them to eat it!\"
+				};
 			}
 
 			await sb.Query.raw([
-				\"INSERT INTO chat_data.Extra_User_Data (User_Alias, Cookie_Today,  Cookie_Gifts_Sent)\", 
-				\"VALUES (\" + extra.user.ID + \", 1, 1)\",
+				\"INSERT INTO chat_data.Extra_User_Data (User_Alias, Cookie_Today,  Cookie_Gifts_Sent)\",
+				\"VALUES (\" + context.user.ID + \", 1, 1)\",
 				\"ON DUPLICATE KEY UPDATE Cookie_Today = 1, Cookie_Gifts_Sent = Cookie_Gifts_Sent + 1\"
 			].join(\" \"));
 
 			await sb.Query.raw([
-				\"UPDATE chat_data.Extra_User_Data\", 
+				\"UPDATE chat_data.Extra_User_Data\",
 				\"SET Cookie_Today = 0, Cookie_Is_Gifted = 1, Cookie_Gifts_Received = Cookie_Gifts_Received + 1\",
 				\"WHERE User_Alias = \" + targetUser.ID
 			].join(\" \"));
-			
-			return { reply: \"Successfully given your cookie for today to \" + targetUser.Name + \" :)\" };	
+
+			return { reply: \"Successfully given your cookie for today to \" + targetUser.Name + \" :)\" };
 		}
 
 		default: return { reply: \"Target type cannot be gifted (yet, at least).\" };
 	}
-}'
+})'
