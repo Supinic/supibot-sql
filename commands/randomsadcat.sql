ed4ebb8ef60e720090e3e99e5290efaca66f606f
Supinic <supinic@protonmail.com>
2020-01-05 19:32:24 +0100

    #suggestion 2211 initial implementation

---
+++ commands/randomsadcat.sql
@@ -0,0 +1,58 @@
+INSERT INTO
+	`chat_data`.`Command`
+	(
+		ID,
+		Name,
+		Aliases,
+		Description,
+		Cooldown,
+		Rollbackable,
+		System,
+		Skip_Banphrases,
+		Whitelisted,
+		Whitelist_Response,
+		Read_Only,
+		Opt_Outable,
+		Blockable,
+		Ping,
+		Pipeable,
+		Archived,
+		Code,
+		Examples,
+		Dynamic_Description
+	)
+VALUES
+	(
+		193,
+		'randomsadcat',
+		'[\"rsc\"]',
+		'Posts a random sad cat image SadCat',
+		15000,
+		0,
+		0,
+		0,
+		0,
+		NULL,
+		0,
+		0,
+		0,
+		1,
+		1,
+		0,
+		'(async function randomSadCat () {
+	const link = sb.Utils.randArray(sb.Config.get(\"SAD_CAT_LINKS\"));
+	return {
+		reply: \"SadCat \" + link
+	};
+})',
+		NULL,
+		NULL
+	)
+
+ON DUPLICATE KEY UPDATE
+	Code = '(async function randomSadCat () {
+	const link = sb.Utils.randArray(sb.Config.get(\"SAD_CAT_LINKS\"));
+	return {
+		reply: \"SadCat \" + link
+	};
+})'
