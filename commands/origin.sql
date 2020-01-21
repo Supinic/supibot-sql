43fc7f9c471c88e16b43dcb6c866d5ddda6e382f
Supinic <supinic@protonmail.com>
2020-01-21 18:39:51 +0100

    #refactor #feature Non-unique emote names support
    Now supports origins of multiple emotes with the same name, using the
    index:X syntax. Also refactored the entire thing to the "modern" style.

--- commands/origin.sql
+++ commands/origin.sql
@@ -39,80 +39,136 @@ VALUES
 		1,
 		1,
 		0,
-		'async (extra, emote) => {
-	if (!emote) {
-		return { reply: \"An emote must be selected first!\" };
-	}
-	else if (emote === \"add\") {
-		return { reply: \"User addition of emote origin is now only possible on https://supinic.com/origin\" };
+		'(async function origin (context, ...args) {
+	if (args.length === 0) {
+		return {
+			reply: \"No input provided!\"
+		};
 	}
 
-	const data = (await sb.Query.getRecordset(rs => rs
+	let emote = null;
+	let customIndex = null;
+	for (let i = args.length - 1; i >= 0; i--) {
+		const token = args[i];
+		if (/^index:\\d+$/.test(token)) {
+			customIndex = Number(token.split(\":\")[1]);
+			args.splice(i, 1);
+		}
+	}
+
+	emote = args.join(\" \");
+	if (emote === null) {
+		return {
+			reply: \"No emote provided!\"
+		};
+	}
+
+	const emoteData = await sb.Query.getRecordset(rs => rs
 		.select(\"Text\", \"Todo\", \"Approved\", \"Emote_Added\")
 		.from(\"data\", \"Origin\")
 		.where(\"Name COLLATE utf8mb4_bin LIKE %s\", emote)
-	))[0];
+	);
 
-	console.log(emote, data);
-
-	if (!data) {
+	if (emoteData.length === 0) {
 		return { reply: \"No definition found for given emote!\" };
 	}
-	else if (data.Approved === \"0\") {
-		return { reply: \"A definition exists, but has not been approved yet...\" };
+	if (emoteData.length > 1 && customIndex === null) {
+		return {
+			reply: `Multiple emotes found for this name! Use \"index:0\" through \"index:${emoteData.length-1}\" to access each one.`
+		};
+	}
+
+	const specificEmoteData = emoteData[customIndex ?? 0];
+	if (!specificEmoteData) {
+		return {
+			reply: \"No emote definition exists for that index!\"
+		};
+	}
+	else if (!specificEmoteData.Approved) {
+		return { reply: \"A definition exists, but has not been approved yet!\" };
 	}
 	else {
-		let string = data.Text;
-	
-		if (data.Emote_Added) {
-			string += \" (Added on \" + data.Emote_Added.sqlDate() + \")\";
+		let string = specificEmoteData.Text;
+
+		if (specificEmoteData.Emote_Added) {
+			string += \" (added on \" + specificEmoteData.Emote_Added.sqlDate() + \")\";
 		}
 
-		if (data.Todo) {
+		if (specificEmoteData.Todo) {
 			string = \"(TODO) \" + string;
 		}
 
-		return { reply: string };
+		return {
+			reply: string
+		};
 	}
-}',
+})',
 		NULL,
 		NULL
 	)
 
 ON DUPLICATE KEY UPDATE
-	Code = 'async (extra, emote) => {
-	if (!emote) {
-		return { reply: \"An emote must be selected first!\" };
-	}
-	else if (emote === \"add\") {
-		return { reply: \"User addition of emote origin is now only possible on https://supinic.com/origin\" };
+	Code = '(async function origin (context, ...args) {
+	if (args.length === 0) {
+		return {
+			reply: \"No input provided!\"
+		};
 	}
 
-	const data = (await sb.Query.getRecordset(rs => rs
+	let emote = null;
+	let customIndex = null;
+	for (let i = args.length - 1; i >= 0; i--) {
+		const token = args[i];
+		if (/^index:\\d+$/.test(token)) {
+			customIndex = Number(token.split(\":\")[1]);
+			args.splice(i, 1);
+		}
+	}
+
+	emote = args.join(\" \");
+	if (emote === null) {
+		return {
+			reply: \"No emote provided!\"
+		};
+	}
+
+	const emoteData = await sb.Query.getRecordset(rs => rs
 		.select(\"Text\", \"Todo\", \"Approved\", \"Emote_Added\")
 		.from(\"data\", \"Origin\")
 		.where(\"Name COLLATE utf8mb4_bin LIKE %s\", emote)
-	))[0];
+	);
 
-	console.log(emote, data);
-
-	if (!data) {
+	if (emoteData.length === 0) {
 		return { reply: \"No definition found for given emote!\" };
 	}
-	else if (data.Approved === \"0\") {
-		return { reply: \"A definition exists, but has not been approved yet...\" };
+	if (emoteData.length > 1 && customIndex === null) {
+		return {
+			reply: `Multiple emotes found for this name! Use \"index:0\" through \"index:${emoteData.length-1}\" to access each one.`
+		};
+	}
+
+	const specificEmoteData = emoteData[customIndex ?? 0];
+	if (!specificEmoteData) {
+		return {
+			reply: \"No emote definition exists for that index!\"
+		};
+	}
+	else if (!specificEmoteData.Approved) {
+		return { reply: \"A definition exists, but has not been approved yet!\" };
 	}
 	else {
-		let string = data.Text;
-	
-		if (data.Emote_Added) {
-			string += \" (Added on \" + data.Emote_Added.sqlDate() + \")\";
+		let string = specificEmoteData.Text;
+
+		if (specificEmoteData.Emote_Added) {
+			string += \" (added on \" + specificEmoteData.Emote_Added.sqlDate() + \")\";
 		}
 
-		if (data.Todo) {
+		if (specificEmoteData.Todo) {
 			string = \"(TODO) \" + string;
 		}
 
-		return { reply: string };
+		return {
+			reply: string
+		};
 	}
-}'
+})'
