e3d68c5f038a96006aaff774b5b24ee6aaa3e58c
Supinic <supinic@protonmail.com>
2020-01-28 19:44:07 +0100

    Initial implementation

---
+++ commands/stachursky.sql
@@ -0,0 +1,56 @@
+INSERT INTO
+	`chat_data`.`Command`
+	(
+		ID,
+		Name,
+		Aliases,
+		Description,
+		Cooldown,
+		Rollbackable,
+		System,
+		Skip_Banphrases,
+		Whitelisted,
+		Whitelist_Response,
+		Read_Only,
+		Opt_Outable,
+		Blockable,
+		Ping,
+		Pipeable,
+		Archived,
+		Code,
+		Examples,
+		Dynamic_Description
+	)
+VALUES
+	(
+		199,
+		'stachursky',
+		NULL,
+		'Posts a random excerpt from a Stachursky song .',
+		10000,
+		0,
+		0,
+		0,
+		0,
+		NULL,
+		0,
+		0,
+		0,
+		1,
+		1,
+		0,
+		'(async function stachursky () {
+	return {
+		reply: sb.Utils.randArray(sb.Config.get(\"STACHURSKY_QUOTES\"))
+	};
+})',
+		NULL,
+		NULL
+	)
+
+ON DUPLICATE KEY UPDATE
+	Code = '(async function stachursky () {
+	return {
+		reply: sb.Utils.randArray(sb.Config.get(\"STACHURSKY_QUOTES\"))
+	};
+})'
