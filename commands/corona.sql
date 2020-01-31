4d3c8e9c81dc919ee4553344333e7a851c315191
Supinic <supinic@protonmail.com>
2020-01-31 20:04:02 +0100

    #refactor Now uses a self-managed cache
    Also added support for country specific data

--- commands/corona.sql
+++ commands/corona.sql
@@ -39,40 +39,156 @@ VALUES
 		1,
 		1,
 		0,
-		'(async function corona () {
-	const makeURL = (type) => `https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&outFields=*&outStatistics=%5B%7B\"statisticType\"%3A\"sum\"%2C\"onStatisticField\"%3A\"${type}\"%2C\"outStatisticFieldName\"%3A\"value\"%7D%5D`;
+		'(async function corona (context, ...args) {
+	if (!this.data.cache || sb.Date.now() > this.data.nextReload) {
+		const url = \"https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/2/query?\";
+		const params = new sb.URLParams()
+			.set(\"f\", \"json\")
+			.set(\"where\", \"1=1\")
+			.set(\"returnGeometry\", \"false\")
+			.set(\"outFields\", \"Confirmed,Deaths,Recovered,Country_Region,Last_Update\");
 
-	const [confirmData, deathData] = await Promise.all([
-		sb.Utils.request(makeURL(\"Confirmed\")),
-		sb.Utils.request(makeURL(\"Deaths\"))
-	]);
+		const data = JSON.parse(await sb.Utils.request({
+			url: url + params.toString()
+		})).features.map(i => i.attributes);
 
-	console.log({confirmData, deathData});
+		let confirmed = 0;
+		let deaths = 0;
+		let recovered = 0;
+		let lastUpdate = -Infinity;
 
-	const cases = JSON.parse(confirmData).features[0].attributes.value;
-	const deaths = JSON.parse(deathData).features[0].attributes.value;
-	return {
-		reply: `Current stats for the Corona virus: ${cases} confirmed cases, ${deaths} deceased.`
-	};
+		this.data.cache = data.map(i => {
+			if (i.Last_Update > lastUpdate) {
+				lastUpdate = i.Last_Update;
+			}
+
+			confirmed += i.Confirmed ?? 0;
+			deaths += i.Deaths ?? 0;
+			recovered += i.Recovered ?? 0;
+
+			if (i.Country_Region === \"US\") {
+				i.Country_Region = \"USA\";
+			}
+
+			return {
+				region: i.Country_Region.trim(),
+				confirmed: i.Confirmed,
+				deaths: i.Deaths,
+				recovered: i.Recovered,
+				lastUpdate: i.Last_Update
+			};
+		});
+
+		this.data.cache.push({
+			region: \"Total\",
+			confirmed,
+			deaths,
+			recovered,
+			lastUpdate
+		});
+
+		this.data.nextReload = new sb.Date().addHours(1).valueOf();
+	}
+
+	const data = this.data.cache;
+	const country = (args.length > 0)
+		? args.join(\" \").toLowerCase()
+		: \"total\";
+
+	const countryData = data.find(i => i.region.toLowerCase().includes(country));
+	if (countryData) {
+		const delta = sb.Utils.timeDelta(new sb.Date(countryData.lastUpdate));
+		const { confirmed, deaths, recovered, region } = countryData;
+		const prefix = (region === \"Total\")
+			? \"In total, there are \"
+			: `${region} has `;
+
+		return {
+			reply: `${prefix} ${confirmed} confirmed case(s), ${deaths ?? \"no\"} death(s) and ${recovered ?? \"no\"} recovered case(s). Last update: ${delta}.`
+		};
+	}
+	else {
+		return {
+			reply: \"That country has no Corona virus data available.\"
+		};
+	}
 })',
 		NULL,
 		NULL
 	)
 
 ON DUPLICATE KEY UPDATE
-	Code = '(async function corona () {
-	const makeURL = (type) => `https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&outFields=*&outStatistics=%5B%7B\"statisticType\"%3A\"sum\"%2C\"onStatisticField\"%3A\"${type}\"%2C\"outStatisticFieldName\"%3A\"value\"%7D%5D`;
+	Code = '(async function corona (context, ...args) {
+	if (!this.data.cache || sb.Date.now() > this.data.nextReload) {
+		const url = \"https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/2/query?\";
+		const params = new sb.URLParams()
+			.set(\"f\", \"json\")
+			.set(\"where\", \"1=1\")
+			.set(\"returnGeometry\", \"false\")
+			.set(\"outFields\", \"Confirmed,Deaths,Recovered,Country_Region,Last_Update\");
 
-	const [confirmData, deathData] = await Promise.all([
-		sb.Utils.request(makeURL(\"Confirmed\")),
-		sb.Utils.request(makeURL(\"Deaths\"))
-	]);
+		const data = JSON.parse(await sb.Utils.request({
+			url: url + params.toString()
+		})).features.map(i => i.attributes);
 
-	console.log({confirmData, deathData});
+		let confirmed = 0;
+		let deaths = 0;
+		let recovered = 0;
+		let lastUpdate = -Infinity;
 
-	const cases = JSON.parse(confirmData).features[0].attributes.value;
-	const deaths = JSON.parse(deathData).features[0].attributes.value;
-	return {
-		reply: `Current stats for the Corona virus: ${cases} confirmed cases, ${deaths} deceased.`
-	};
+		this.data.cache = data.map(i => {
+			if (i.Last_Update > lastUpdate) {
+				lastUpdate = i.Last_Update;
+			}
+
+			confirmed += i.Confirmed ?? 0;
+			deaths += i.Deaths ?? 0;
+			recovered += i.Recovered ?? 0;
+
+			if (i.Country_Region === \"US\") {
+				i.Country_Region = \"USA\";
+			}
+
+			return {
+				region: i.Country_Region.trim(),
+				confirmed: i.Confirmed,
+				deaths: i.Deaths,
+				recovered: i.Recovered,
+				lastUpdate: i.Last_Update
+			};
+		});
+
+		this.data.cache.push({
+			region: \"Total\",
+			confirmed,
+			deaths,
+			recovered,
+			lastUpdate
+		});
+
+		this.data.nextReload = new sb.Date().addHours(1).valueOf();
+	}
+
+	const data = this.data.cache;
+	const country = (args.length > 0)
+		? args.join(\" \").toLowerCase()
+		: \"total\";
+
+	const countryData = data.find(i => i.region.toLowerCase().includes(country));
+	if (countryData) {
+		const delta = sb.Utils.timeDelta(new sb.Date(countryData.lastUpdate));
+		const { confirmed, deaths, recovered, region } = countryData;
+		const prefix = (region === \"Total\")
+			? \"In total, there are \"
+			: `${region} has `;
+
+		return {
+			reply: `${prefix} ${confirmed} confirmed case(s), ${deaths ?? \"no\"} death(s) and ${recovered ?? \"no\"} recovered case(s). Last update: ${delta}.`
+		};
+	}
+	else {
+		return {
+			reply: \"That country has no Corona virus data available.\"
+		};
+	}
 })'
