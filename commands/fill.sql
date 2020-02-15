356c6df8fc9134c33e38db700000efb4cc03e663
Supinic <supinic@protonmail.com>
2020-02-15 20:07:57 +0100

    removed the online character limit

--- commands/fill.sql
+++ commands/fill.sql
@@ -52,7 +52,7 @@ VALUES
 
 	let limit = (context.channel?.Message_Limit ?? sb.Config.get(\"DEFAULT_MSG_LIMIT_\" + platform)) - context.user.Name.length - 3;
 	if (context.channel && context.channel.sessionData.live) {
-		limit = 150;
+		// limit = 150;
 	}
 
 	while (length < limit) {
@@ -98,7 +98,7 @@ ON DUPLICATE KEY UPDATE
 
 	let limit = (context.channel?.Message_Limit ?? sb.Config.get(\"DEFAULT_MSG_LIMIT_\" + platform)) - context.user.Name.length - 3;
 	if (context.channel && context.channel.sessionData.live) {
-		limit = 150;
+		// limit = 150;
 	}
 
 	while (length < limit) {
