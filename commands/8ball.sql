85acbe02f82162beb943a77435d8dd2e9eb9001c
Supinic <supinic@protonmail.com>
2020-03-27 19:13:06 +0100

    transferred a Config variable into staticData

--- commands/8ball.sql
+++ commands/8ball.sql
@@ -40,10 +40,35 @@ VALUES
 		1,
 		1,
 		0,
-		NULL,
+		'({
+	responses: [
+		\"😃 It is certain.\",
+		\"😃 It is decidedly so.\",
+		\"😃 Without a doubt.\",
+		\"😃 Yes - definitely.\",
+		\"😃 You may rely on it.\",
+		\"😃 As I see it, yes.\",
+		\"😃 Most likely.\",
+		\"😃 Outlook good.\",
+		\"😃 Yes.\",
+		\"😃 Signs point to yes.\",
+		
+		\"😐 Reply hazy, try again.\",
+		\"😐 Ask again later.\",
+		\"😐 Better not tell you now.\",
+		\"😐 Cannot predict now.\",
+		\"😐 Concentrate and ask again.\",
+		
+		\"😦 Don\'t count on it.\",
+		\"😦 My reply is no.\",
+		\"😦 My sources say no.\",
+		\"😦 Outlook not so good.\",
+		\"😦 Very doubtful.\"
+	]
+})',
 		'(async function _8ball () {
 	return {
-		reply: sb.Utils.randArray(sb.Config.get(\"8_BALL_RESPONSES\"))
+		reply: sb.Utils.randArray(this.staticData.responses)
 	};
 })',
 		NULL,
@@ -59,6 +84,6 @@ VALUES
 ON DUPLICATE KEY UPDATE
 	Code = '(async function _8ball () {
 	return {
-		reply: sb.Utils.randArray(sb.Config.get(\"8_BALL_RESPONSES\"))
+		reply: sb.Utils.randArray(this.staticData.responses)
 	};
 })'
