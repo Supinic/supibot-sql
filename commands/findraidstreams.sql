4875df1dfab994877256d6a18e91b07c8db625a5
Supinic <supinic@protonmail.com>
2020-04-01 18:27:40 +0200

    added option of ignoring channels and a viewer threshold

--- commands/findraidstreams.sql
+++ commands/findraidstreams.sql
@@ -40,18 +40,28 @@ VALUES
 		0,
 		1,
 		0,
-		NULL,
+		'({
+	viewerThreshold: 100,
+	ignoredChannels: [ 42 ]
+})',
 		'(async function findRaidStreams () {
-	const raidable = sb.Channel.data.filter(i => i.sessionData?.live).map(channel => {
-		const { stream } = channel.sessionData;
-		return {
-			name: channel.Name,
-			game: stream.game,
-			status: stream.status,
-			viewers: stream.viewers,
-			online: sb.Utils.timeDelta(stream.since, true)
-		};
-	}).sort((a, b) => b.viewers - a.viewers);
+	const raidable = sb.Channel.data
+		.filter(i => (
+			i.sessionData?.live
+			&& !this.staticData.ignoredChannels.includes(i.ID)
+		))
+		.map(channel => {
+			const { stream } = channel.sessionData;
+			return {
+				name: channel.Name,
+				game: stream.game,
+				status: stream.status,
+				viewers: stream.viewers,
+				online: sb.Utils.timeDelta(stream.since, true)
+			};
+		})
+		.filter(i => i.viewers < this.staticData.viewerThreshold)
+		.sort((a, b) => b.viewers - a.viewers);
 
 	return {
 		reply: await sb.Pastebin.post(JSON.stringify(raidable, null, 4), {
@@ -66,16 +76,23 @@ VALUES
 
 ON DUPLICATE KEY UPDATE
 	Code = '(async function findRaidStreams () {
-	const raidable = sb.Channel.data.filter(i => i.sessionData?.live).map(channel => {
-		const { stream } = channel.sessionData;
-		return {
-			name: channel.Name,
-			game: stream.game,
-			status: stream.status,
-			viewers: stream.viewers,
-			online: sb.Utils.timeDelta(stream.since, true)
-		};
-	}).sort((a, b) => b.viewers - a.viewers);
+	const raidable = sb.Channel.data
+		.filter(i => (
+			i.sessionData?.live
+			&& !this.staticData.ignoredChannels.includes(i.ID)
+		))
+		.map(channel => {
+			const { stream } = channel.sessionData;
+			return {
+				name: channel.Name,
+				game: stream.game,
+				status: stream.status,
+				viewers: stream.viewers,
+				online: sb.Utils.timeDelta(stream.since, true)
+			};
+		})
+		.filter(i => i.viewers < this.staticData.viewerThreshold)
+		.sort((a, b) => b.viewers - a.viewers);
 
 	return {
 		reply: await sb.Pastebin.post(JSON.stringify(raidable, null, 4), {
