36d82f3049554bb3896203378b24883682daf5aa
Supinic <supinic@protonmail.com>
2020-04-07 21:44:39 +0200

    reformatted to use the new UserData location

--- commands/time.sql
+++ commands/time.sql
@@ -40,103 +40,132 @@ VALUES
 		1,
 		1,
 		0,
-		NULL,
-		'(async function time (context, ...args) {
-	let skipLocation = false;
+		'({
+	detectTimezone: async (...args) => {
+		const place = args.join(\" \");
+		const timezone = await sb.Query.getRecordset(rs => rs
+			.select(\"Abbreviation\", \"Offset\", \"Name\")
+			.from(\"data\", \"Timezone\")
+			.where(\"Abbreviation = %s\", place)
+			.limit(1)
+			.single()
+		);
 
-	if (args.length === 0) {
-		if (context.user.Data.defaultLocation) {
-			args = context.user.Data.defaultLocation.address;
-			skipLocation = context.user.Data.defaultLocation.private;
+		if (!timezone) {
+			return null;
 		}
-		else {
-			return {
-				reply: \"You must search for something first!\",
-				cooldown: { length: 2500 }
-			};
-		}
-	}
-	else if (args[0].toLowerCase().replace(/^@/, \"\") === sb.Config.get(\"SELF\")) {
-		return {
-			reply: \"🤖 My current time: \" + sb.Date.now() + \" 🤖\"
-		};
-	}
-	else if (args[0].startsWith(\"@\") ) {
-		const targetUser = await sb.User.get(args[0]);
-		if (!targetUser) {
-			return {
-				reply: \"That user does not exist!\",
-				cooldown: { length: 2500 }
-			};
-		}
-		else if (!targetUser.Data.defaultLocation) {
-			return {
-				reply: \"That user has not set their default location!\",
-				cooldown: { length: 2500 }
-			};
-		}
-		else {
-			args = targetUser.Data.defaultLocation.address;
-			skipLocation = targetUser.Data.defaultLocation.private;
-		}
-	}
 
-	const place = args.join(\" \");
-	const timezone = await sb.Query.getRecordset(rs => rs
-		.select(\"Abbreviation\", \"Offset\", \"Name\")
-		.from(\"data\", \"Timezone\")
-		.where(\"Abbreviation = %s\", place)
-		.limit(1)
-		.single()
-	);
-	if (timezone) {
 		const extraOffset = (Math.trunc(timezone.Offset) - timezone.Offset) * 60;
-		let prettyOffset = (\"00\" + String(Math.trunc(timezone.Offset))).slice(-2) + \":\" + (\"00\" + extraOffset).slice(-2);
-		let extra = \"\";
-
-		if (prettyOffset[0] !== \"-\") {
-			prettyOffset = \"+\" + prettyOffset;
-		}
-
-		if (timezone.Abbreviation === \"UTC\") {
-			extra = \"JUST 4Head\";
-			prettyOffset = \"\";
+		let offset = (\"00\" + String(Math.trunc(timezone.Offset))).slice(-2) + \":\" + (\"00\" + extraOffset).slice(-2);
+		if (offset[0] !== \"-\") {
+			offset = \"+\" + offset;
 		}
 
 		const date = new sb.Date().setTimezoneOffset(timezone.Offset * 60).format(\"H:i (Y-m-d)\");
 		return {
-			reply: `TIMEZONEDETECTED ${timezone.Abbreviation} is ${timezone.Name}, which is ${extra} UTC${prettyOffset} and it is ${date} there right now.`
+			date,
+			offset,
+			abbr: timezone.Abbreviation,
+			name: timezone.Name,
+		};
+	}
+})',
+		'(async function time (context, ...args) {
+	const zone = await this.staticData.detectTimezone(...args);
+	if (zone) {
+		return {
+			reply: `TIMEZONEDETECTED ${zone.abbr} is ${zone.name}, which is UTC${zone.offset} and it is ${zone.date} there right now.`
 		};
 	}
 
-	const { results: [geoData] } = await sb.Got.instances.Google({
-		url: \"geocode/json\",
-		searchParams: new sb.URLParams()
-			.set(\"address\", place)
-			.set(\"key\", sb.Config.get(\"API_GOOGLE_GEOCODING\"))
-			.toString()
-	}).json();
+	let skipLocation = false;
+	let coordinates = null;
+	let place = args.join(\" \");
 
-	if (!geoData) {
-		return { reply: \"No place matching that query has been found!\" };
+	if (args.length === 0) {
+		if (context.user.Data.location) {
+			coordinates = context.user.Data.location.coordinates;
+			skipLocation = context.user.Data.location.hidden;
+			place = context.user.Data.location.formatted;
+		}
+		else {
+			return {
+				success: false,
+				reply: \"You must search for something first, or set your default location!\",
+				cooldown: 2500
+			};
+		}
+	}
+	else if (args[0].startsWith(\"@\")) {
+		const targetUser = await sb.User.get(args[0]);
+		if (!targetUser) {
+			return {
+				success: false,
+				reply: \"That user does not exist!\",
+				cooldown: 2500
+			};
+		}
+		else if (targetUser.ID === sb.Config.get(\"SELF_ID\")) {
+			return {
+				reply: `My current time is ${sb.Date.now()} 🤖`
+			};
+		}
+		else if (!targetUser.Data.location) {
+			return {
+				success: false,
+				reply: \"That user has not set their location!\",
+				cooldown: 2500
+			};
+		}
+		else {
+			coordinates = targetUser.Data.location.coordinates;
+			skipLocation = targetUser.Data.location.hidden;
+			place = targetUser.Data.location.formatted;
+		}
+	}
+
+	if (coordinates === null) {
+		const { results: [geoData] } = await sb.Got.instances.Google({
+			url: \"geocode/json\",
+			searchParams: new sb.URLParams()
+				.set(\"address\", place)
+				.set(\"key\", sb.Config.get(\"API_GOOGLE_GEOCODING\"))
+				.toString()
+		}).json();
+
+		if (!geoData) {
+			return {
+				success: false,
+				reply: \"No place matching that query has been found!\"
+			};
+		}
+		else {
+			coordinates = geoData.geometry.location;
+		}
 	}
 
 	const timeData = await sb.Got.instances.Google({
 		url: \"timezone/json\",
 		searchParams: new sb.URLParams()
 			.set(\"timestamp\", Math.trunc(sb.Date.now() / 1000).toString())
-			.set(\"location\", geoData.geometry.location.lat + \",\" + geoData.geometry.location.lng)
+			.set(\"location\", coordinates.lat + \",\" + coordinates.lng)
 			.set(\"key\", sb.Config.get(\"API_GOOGLE_TIMEZONE\"))
 			.toString()
 	}).json();
 
 	if (timeData.status === \"ZERO_RESULTS\") {
-		return { reply: \"Target place is ambiguous - it contains more than one timezone\\)!\" };
+		return {
+			success: false,
+			reply: \"Target place is ambiguous - it contains more than one timezone!\"
+		};
 	}
 
 	const totalOffset = (timeData.rawOffset + timeData.dstOffset);
-	const offset = (totalOffset >= 0 ? \"+\" : \"-\") + Math.trunc(Math.abs(totalOffset) / 3600) + \":\" + sb.Utils.zf((Math.abs(totalOffset) % 3600) / 60, 2);
+	const symbol = (totalOffset >= 0 ? \"+\" : \"-\");
+	const hours = Math.trunc(Math.abs(totalOffset) / 3600);
+	const minutes = sb.Utils.zf((Math.abs(totalOffset) % 3600) / 60, 2);
 
+	const offset = `${symbol}${hours}:${minutes}`;
 	const time = new sb.Date();
 	time.setTimezoneOffset(totalOffset / 60);
 
@@ -151,101 +180,101 @@ VALUES
 
 ON DUPLICATE KEY UPDATE
 	Code = '(async function time (context, ...args) {
+	const zone = await this.staticData.detectTimezone(...args);
+	if (zone) {
+		return {
+			reply: `TIMEZONEDETECTED ${zone.abbr} is ${zone.name}, which is UTC${zone.offset} and it is ${zone.date} there right now.`
+		};
+	}
+
 	let skipLocation = false;
+	let coordinates = null;
+	let place = args.join(\" \");
 
 	if (args.length === 0) {
-		if (context.user.Data.defaultLocation) {
-			args = context.user.Data.defaultLocation.address;
-			skipLocation = context.user.Data.defaultLocation.private;
+		if (context.user.Data.location) {
+			coordinates = context.user.Data.location.coordinates;
+			skipLocation = context.user.Data.location.hidden;
+			place = context.user.Data.location.formatted;
 		}
 		else {
 			return {
-				reply: \"You must search for something first!\",
-				cooldown: { length: 2500 }
+				success: false,
+				reply: \"You must search for something first, or set your default location!\",
+				cooldown: 2500
 			};
 		}
 	}
-	else if (args[0].toLowerCase().replace(/^@/, \"\") === sb.Config.get(\"SELF\")) {
-		return {
-			reply: \"🤖 My current time: \" + sb.Date.now() + \" 🤖\"
-		};
-	}
-	else if (args[0].startsWith(\"@\") ) {
+	else if (args[0].startsWith(\"@\")) {
 		const targetUser = await sb.User.get(args[0]);
 		if (!targetUser) {
 			return {
+				success: false,
 				reply: \"That user does not exist!\",
-				cooldown: { length: 2500 }
+				cooldown: 2500
 			};
 		}
-		else if (!targetUser.Data.defaultLocation) {
+		else if (targetUser.ID === sb.Config.get(\"SELF_ID\")) {
 			return {
-				reply: \"That user has not set their default location!\",
-				cooldown: { length: 2500 }
+				reply: `My current time is ${sb.Date.now()} 🤖`
+			};
+		}
+		else if (!targetUser.Data.location) {
+			return {
+				success: false,
+				reply: \"That user has not set their location!\",
+				cooldown: 2500
 			};
 		}
 		else {
-			args = targetUser.Data.defaultLocation.address;
-			skipLocation = targetUser.Data.defaultLocation.private;
+			coordinates = targetUser.Data.location.coordinates;
+			skipLocation = targetUser.Data.location.hidden;
+			place = targetUser.Data.location.formatted;
 		}
 	}
 
-	const place = args.join(\" \");
-	const timezone = await sb.Query.getRecordset(rs => rs
-		.select(\"Abbreviation\", \"Offset\", \"Name\")
-		.from(\"data\", \"Timezone\")
-		.where(\"Abbreviation = %s\", place)
-		.limit(1)
-		.single()
-	);
-	if (timezone) {
-		const extraOffset = (Math.trunc(timezone.Offset) - timezone.Offset) * 60;
-		let prettyOffset = (\"00\" + String(Math.trunc(timezone.Offset))).slice(-2) + \":\" + (\"00\" + extraOffset).slice(-2);
-		let extra = \"\";
+	if (coordinates === null) {
+		const { results: [geoData] } = await sb.Got.instances.Google({
+			url: \"geocode/json\",
+			searchParams: new sb.URLParams()
+				.set(\"address\", place)
+				.set(\"key\", sb.Config.get(\"API_GOOGLE_GEOCODING\"))
+				.toString()
+		}).json();
 
-		if (prettyOffset[0] !== \"-\") {
-			prettyOffset = \"+\" + prettyOffset;
+		if (!geoData) {
+			return {
+				success: false,
+				reply: \"No place matching that query has been found!\"
+			};
 		}
-
-		if (timezone.Abbreviation === \"UTC\") {
-			extra = \"JUST 4Head\";
-			prettyOffset = \"\";
+		else {
+			coordinates = geoData.geometry.location;
 		}
-
-		const date = new sb.Date().setTimezoneOffset(timezone.Offset * 60).format(\"H:i (Y-m-d)\");
-		return {
-			reply: `TIMEZONEDETECTED ${timezone.Abbreviation} is ${timezone.Name}, which is ${extra} UTC${prettyOffset} and it is ${date} there right now.`
-		};
-	}
-
-	const { results: [geoData] } = await sb.Got.instances.Google({
-		url: \"geocode/json\",
-		searchParams: new sb.URLParams()
-			.set(\"address\", place)
-			.set(\"key\", sb.Config.get(\"API_GOOGLE_GEOCODING\"))
-			.toString()
-	}).json();
-
-	if (!geoData) {
-		return { reply: \"No place matching that query has been found!\" };
 	}
 
 	const timeData = await sb.Got.instances.Google({
 		url: \"timezone/json\",
 		searchParams: new sb.URLParams()
 			.set(\"timestamp\", Math.trunc(sb.Date.now() / 1000).toString())
-			.set(\"location\", geoData.geometry.location.lat + \",\" + geoData.geometry.location.lng)
+			.set(\"location\", coordinates.lat + \",\" + coordinates.lng)
 			.set(\"key\", sb.Config.get(\"API_GOOGLE_TIMEZONE\"))
 			.toString()
 	}).json();
 
 	if (timeData.status === \"ZERO_RESULTS\") {
-		return { reply: \"Target place is ambiguous - it contains more than one timezone\\)!\" };
+		return {
+			success: false,
+			reply: \"Target place is ambiguous - it contains more than one timezone!\"
+		};
 	}
 
 	const totalOffset = (timeData.rawOffset + timeData.dstOffset);
-	const offset = (totalOffset >= 0 ? \"+\" : \"-\") + Math.trunc(Math.abs(totalOffset) / 3600) + \":\" + sb.Utils.zf((Math.abs(totalOffset) % 3600) / 60, 2);
+	const symbol = (totalOffset >= 0 ? \"+\" : \"-\");
+	const hours = Math.trunc(Math.abs(totalOffset) / 3600);
+	const minutes = sb.Utils.zf((Math.abs(totalOffset) % 3600) / 60, 2);
 
+	const offset = `${symbol}${hours}:${minutes}`;
 	const time = new sb.Date();
 	time.setTimezoneOffset(totalOffset / 60);
 
