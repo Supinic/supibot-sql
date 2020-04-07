b1d9d5272e8e179913df67bcff34e880996ed5cc
Supinic <supinic@protonmail.com>
2020-04-08 01:23:18 +0200

    initial implementation, might be archived soon

---
+++ commands/duckduckgo.sql
@@ -0,0 +1,110 @@
+INSERT INTO
+	`chat_data`.`Command`
+	(
+		ID,
+		Name,
+		Aliases,
+		Description,
+		Cooldown,
+		Rollbackable,
+		System,
+		Skip_Banphrases,
+		Whitelisted,
+		Whitelist_Response,
+		Read_Only,
+		Opt_Outable,
+		Blockable,
+		Ping,
+		Pipeable,
+		Archived,
+		Static_Data,
+		Code,
+		Examples,
+		Dynamic_Description
+	)
+VALUES
+	(
+		209,
+		'duckduckgo',
+		'[\"ddg\"]',
+		'Try the DuckDuckGo Instant Answer API to answer your questions!',
+		15000,
+		0,
+		0,
+		0,
+		0,
+		NULL,
+		0,
+		0,
+		0,
+		1,
+		1,
+		0,
+		NULL,
+		'(async function duckDuckGo (context, ...args) {
+	const data = await sb.Got({
+		url: \"https://api.duckduckgo.com/\",
+		searchParams: new sb.URLParams()
+			.set(\"format\", \"json\")
+			.set(\"pretty\", \"1\")
+			.set(\"q\", args.join(\" \"))
+			.toString()
+	}).json();
+
+	const {
+		Heading: heading,
+		AbstractSource: source,
+		AbstractURL: url,
+		Abstract: abstract		
+	} = data;
+
+	if (!abstract) {
+		return {
+			reply: \"No data found!\"
+		};
+	}
+	
+	return {
+		reply: sb.Utils.tag.trim `
+			From ${source} -
+			${heading}:
+			${abstract}			
+		`
+	};	
+})',
+		NULL,
+		NULL
+	)
+
+ON DUPLICATE KEY UPDATE
+	Code = '(async function duckDuckGo (context, ...args) {
+	const data = await sb.Got({
+		url: \"https://api.duckduckgo.com/\",
+		searchParams: new sb.URLParams()
+			.set(\"format\", \"json\")
+			.set(\"pretty\", \"1\")
+			.set(\"q\", args.join(\" \"))
+			.toString()
+	}).json();
+
+	const {
+		Heading: heading,
+		AbstractSource: source,
+		AbstractURL: url,
+		Abstract: abstract		
+	} = data;
+
+	if (!abstract) {
+		return {
+			reply: \"No data found!\"
+		};
+	}
+	
+	return {
+		reply: sb.Utils.tag.trim `
+			From ${source} -
+			${heading}:
+			${abstract}			
+		`
+	};	
+})'
