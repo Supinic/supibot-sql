dea16b195a552b302841299122c7602e0c225662
Supinic <supinic@protonmail.com>
2020-04-28 19:29:22 +0200

    moved playlist fetching to new method in Utils

--- commands/randomzizarandeath.sql
+++ commands/randomzizarandeath.sql
@@ -44,41 +44,30 @@ VALUES
 		1,
 		0,
 		0,
-		NULL,
+		'({
+	playlist: \"PLbpExg9_Xax24tS9rNt8IP49VFFaDghAG\"
+})',
 		'(async function randomZizaranDeath () {
 	if (!this.data.videoList) {
-		const playlistID = \"PLbpExg9_Xax24tS9rNt8IP49VFFaDghAG\";
-		const videos = [];
-		let pageToken = null;
+		const { result, reason, success } = await sb.Utils.fetchYoutubePlaylist({
+			key: sb.Config.get(\"API_GOOGLE_YOUTUBE\"),
+   			playlistID: this.staticData.playlist
+		});
 
-		do {
-			// implement and then use sb.URLParams.prototype.clone here
-			const params = new sb.URLParams()
-				.set(\"part\", \"snippet\")
-				.set(\"maxResults\", 50)
-				.set(\"key\", sb.Config.get(\"API_GOOGLE_YOUTUBE\"))
-				.set(\"playlistId\", \"PLbpExg9_Xax24tS9rNt8IP49VFFaDghAG\");
-
-			if (pageToken) {
-				params.set(\"pageToken\", pageToken);
-			}
-			
-			const playlistData = await sb.Got({
-				url: \"https://www.googleapis.com/youtube/v3/playlistItems\",
-				searchParams: params.toString()
-			}).json();
-			
-			pageToken = playlistData.nextPageToken;
-			videos.push(...playlistData.items.map(i => i.snippet.resourceId.videoId));
-
-		} while (pageToken);
-
-		this.data.videoList = videos;
+		if (!success) {
+			return {
+				success,
+				reply: `Playlist could not be fetched! Reason: ${reason}`
+			};
+		}
+		else {
+			this.data.videoList = result;
+		}
 	}
 
 	const video = sb.Utils.randArray(this.data.videoList);
 	return {
-		reply: `PepeLaugh 👉 https://youtu.be/${video}`
+		reply: `PepeLaugh 👉 https://youtu.be/${video.ID}`
 	};
 })',
 		NULL,
@@ -88,37 +77,24 @@ VALUES
 ON DUPLICATE KEY UPDATE
 	Code = '(async function randomZizaranDeath () {
 	if (!this.data.videoList) {
-		const playlistID = \"PLbpExg9_Xax24tS9rNt8IP49VFFaDghAG\";
-		const videos = [];
-		let pageToken = null;
+		const { result, reason, success } = await sb.Utils.fetchYoutubePlaylist({
+			key: sb.Config.get(\"API_GOOGLE_YOUTUBE\"),
+   			playlistID: this.staticData.playlist
+		});
 
-		do {
-			// implement and then use sb.URLParams.prototype.clone here
-			const params = new sb.URLParams()
-				.set(\"part\", \"snippet\")
-				.set(\"maxResults\", 50)
-				.set(\"key\", sb.Config.get(\"API_GOOGLE_YOUTUBE\"))
-				.set(\"playlistId\", \"PLbpExg9_Xax24tS9rNt8IP49VFFaDghAG\");
-
-			if (pageToken) {
-				params.set(\"pageToken\", pageToken);
-			}
-			
-			const playlistData = await sb.Got({
-				url: \"https://www.googleapis.com/youtube/v3/playlistItems\",
-				searchParams: params.toString()
-			}).json();
-			
-			pageToken = playlistData.nextPageToken;
-			videos.push(...playlistData.items.map(i => i.snippet.resourceId.videoId));
-
-		} while (pageToken);
-
-		this.data.videoList = videos;
+		if (!success) {
+			return {
+				success,
+				reply: `Playlist could not be fetched! Reason: ${reason}`
+			};
+		}
+		else {
+			this.data.videoList = result;
+		}
 	}
 
 	const video = sb.Utils.randArray(this.data.videoList);
 	return {
-		reply: `PepeLaugh 👉 https://youtu.be/${video}`
+		reply: `PepeLaugh 👉 https://youtu.be/${video.ID}`
 	};
 })'
