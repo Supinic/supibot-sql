9794d33f799600d0eed1ea00d108e11ebd242464
Supinic <supinic@protonmail.com>
2020-05-06 23:32:57 +0200

    initial commit - cron to refresh twitch app access tokens
    runs once every day, at noon

---
+++ crons/refresh-twitch-app-access-token.sql
@@ -0,0 +1,37 @@
+INSERT INTO
+	`chat_data`.`Cron`
+	(
+		ID,
+		Name,
+		Expression,
+		Description,
+		Code,
+		Type,
+		Active
+	)
+VALUES
+	(
+		18,
+		'refresh-twitch-app-access-token',
+		'0 0 12 * * *',
+		'Refreshes the bot\'s Twitch app access token.',
+		'(async function cron_refreshTwitchAppAccessToken () {
+	const { access_token: token } = await sb.Got({
+		method: \"POST\",
+		url: \"https://id.twitch.tv/oauth2/token\",
+		responseType: \"json\",
+		searchParams: new sb.URLParams()
+			.set(\"grant_type\", \"client_credentials\")
+			.set(\"client_id\", sb.Config.get(\"TWITCH_CLIENT_ID\"))
+			.set(\"client_secret\", sb.Config.get(\"TWITCH_CLIENT_SECRET\"))
+			.set(\"scope\", \"\")
+			.toString()
+	}).json();
+
+	await sb.Config.set(\"TWITCH_APP_ACCESS_TOKEN\", token);
+	
+	console.log(\"Twitch app access token successfuly updated\");
+})',
+		'Bot',
+		1
+	)
