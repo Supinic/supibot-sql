917e16b15496f0b6aeb84d6d1bb522a256bc4472
Supinic <supinic@protonmail.com>
2020-05-15 19:31:51 +0200

    initial commit

---
+++ commands/freeforsen.sql
@@ -0,0 +1,42 @@
+INSERT INTO
+	`chat_data`.`Command`
+	(
+		ID,
+		Name,
+		Aliases,
+		Flags,
+		Description,
+		Cooldown,
+		Whitelist_Response,
+		Static_Data,
+		Code,
+		Dynamic_Description
+	)
+VALUES
+	(
+		219,
+		'freeforsen',
+		NULL,
+		'ping,pipe',
+		'Free Forsen forsenE',
+		10000,
+		NULL,
+		'({
+	ban: new sb.Date(\"2020-05-08 20:12:52\"),
+	expiry: new sb.Date(\"2020-05-08 20:12:52\").addDays(14)
+})',
+		'(async function () {
+	const { ban, expiry } = this.staticData;
+	const banDelta = sb.Utils.timeDelta(ban);
+	const expiryDelta = sb.Utils.timeDelta(expiry);
+
+	const string = (expiry <= new sb.Date()) 
+		? \"has probably been unbanned\"
+		: \"will most likely return\";
+
+	return {
+		reply: `forsenE got banned ${banDelta} and ${string} ${expiryDelta}. forsenW`
+	};
+})',
+		NULL
+	)
