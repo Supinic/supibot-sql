94659a3487eb283078fd32fe22617ee295b1211e
Supinic <supinic@protonmail.com>
2020-05-16 19:28:31 +0200

    complete rework - now restarts along with more commands
    supports git pull, yarn update and then the restart itself

--- commands/restart.sql
+++ commands/restart.sql
@@ -17,14 +17,41 @@ VALUES
 		140,
 		'restart',
 		NULL,
-		'pipe,skip-banphrase,system,whitelist',
+		'read-only,system,whitelist',
 		'Restarts the bot by killing the process and letting PM2 restart it.',
 		0,
 		NULL,
 		NULL,
-		'(async function restart () {
-	setTimeout(() => process.abort(), 1000);
-	return { reply: \"Restarting...\" };
-})',
+		'(async function restart (context, ...types) {
+	const { promisify } = require(\"util\");
+	const shell = promisify(require(\"child_process\").exec);
+
+	types = types.map(i => i.toLowerCase());
+
+	const queue = [];
+	if (types.includes(\"all\") || types.includes(\"pull\")) {
+		queue.push(async () => {
+			sb.Master.send(\"Pulling from master PogChamp 👉\", context.channel);
+			const result = await shell(\"git pull origin master\");
+			console.log(\"pull result\", result);
+		});
+	}
+	if (types.includes(\"all\") || types.includes(\"yarn\") || types.includes(\"upgrade\")) {
+		queue.push(async () => {
+			sb.Master.send(\"Updating supi-core module PogChamp 👉\", context.channel);
+			const result = await shell(\"yarn upgrade supi-core\");
+			console.log(\"upgrade result\", result);
+		});
+	}
+
+	queue.push(async () => {
+		sb.Master.send(\"Restarting process PogChamp 👉\", context.channel);
+		setTimeout(() => process.abort(), 1000);
+	});
+
+	for (const fn of queue) {
+		await fn();
+	}
+});',
 		NULL
 	)
