906184f4f00d513c8ab2b5fe12646805cb2d1f3b
Supinic <supinic@protonmail.com>
2020-05-27 18:50:05 +0200

    initial implementation

---
+++ commands/randommeal.sql
@@ -0,0 +1,56 @@
+INSERT INTO
+	`chat_data`.`Command`
+	(
+		ID,
+		Name,
+		Aliases,
+		Flags,
+		Description,
+		Cooldown,
+		Whitelist_Response,
+		Static_Data,
+		Code,
+		Dynamic_Description,
+		Source
+	)
+VALUES
+	(
+		223,
+		'randommeal',
+		NULL,
+		'ping,pipe',
+		NULL,
+		0,
+		NULL,
+		NULL,
+		'(async function randomMeal (context, ...args) {
+	let data = null;
+	if (args.length === 0) {
+		data = await sb.Got(\"https://www.themealdb.com/api/json/v1/1/random.php\").json();
+	}
+	else {
+		data = await sb.Got({
+			url: \"https://www.themealdb.com/api/json/v1/1/search.php\",
+			searchParams: new sb.URLParams()
+				.set(\"s\", args.join(\" \"))
+				.toString()
+		}).json();
+
+		if (!data?.meals) {
+			return {
+				success: false,
+				reply: \"No recipes found for that query!\"
+			};
+		}
+	}
+	
+	const meal = data.meals[0];
+	const ingredients = Object.entries(meal).filter(([key, value]) => /ingredient\\d+/i.test(key)).map(([key, value]) => value).filter(Boolean);
+
+	return {
+		reply: `${meal.strMeal} - Ingredients: ${ingredients.join(\", \")} ${meal.strYoutube}`
+	};
+})',
+		NULL,
+		'supinic/supibot-sql'
+	)
