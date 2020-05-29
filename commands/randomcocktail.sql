3210199d53a2dd7c085726d7bcb0104994896f9e
Supinic <supinic@protonmail.com>
2020-05-29 20:41:42 +0200

    initial implementation

---
+++ commands/randomcocktail.sql
@@ -0,0 +1,56 @@
+INSERT INTO
+	`chat_data`.`Command`
+	(
+		ID,
+		Name,
+		Aliases,
+		Flags,
+		Description,
+		Cooldown,
+		Whitelist_Response,
+		Static_Data,
+		Code,
+		Dynamic_Description,
+		Source
+	)
+VALUES
+	(
+		224,
+		'randomcocktail',
+		'[\"cock\", \"drinks\", \"tail\"]',
+		'ping,pipe',
+		'Searches for a cocktail recipe by its name, or fetches a random one, if no search query was provided.',
+		0,
+		NULL,
+		NULL,
+		'(async function randomCocktail (context, ...args) {
+	let data = null;
+	if (args.length === 0) {
+		data = await sb.Got(\"https://www.thecocktaildb.com/api/json/v1/1/random.php\").json();
+	}
+	else {
+		data = await sb.Got({
+			url: \"https://www.thecocktaildb.com/api/json/v1/1/search.php\",
+			searchParams: new sb.URLParams()
+				.set(\"s\", args.join(\" \"))
+				.toString()
+		}).json();
+
+		if (!data?.drinks) {
+			return {
+				success: false,
+				reply: \"No cocktails found for that query!\"
+			};
+		}
+	}
+	
+	const drink = data.drinks[0];
+	const ingredients = Object.entries(drink).filter(([key, value]) => /ingredient\\d+/i.test(key)).map(([key, value]) => value).filter(Boolean);
+
+	return {
+		reply: `${drink.strDrink} (${ingredients.join(\", \")}): ${drink.strInstructions}`
+	};
+})',
+		NULL,
+		'supinic/supibot-sql'
+	)
