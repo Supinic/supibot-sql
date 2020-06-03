5d60a783b14b9b22a527dfc197f919e0ff14c940
Supinic <supinic@protonmail.com>
2020-06-03 17:33:43 +0200

    added subtype "current" to get a timestamp

--- commands/vod.sql
+++ commands/vod.sql
@@ -19,17 +19,23 @@ VALUES
 		'vod',
 		NULL,
 		'ping',
-		'Posts the last VOD of a specified channel (for now)',
+		'Posts the last VOD of a specified channel. If you use the keyword \"current\", you\'ll get a timestamp as well.',
 		15000,
 		NULL,
 		NULL,
-		'(async function vod (context, target) {
+		'(async function vod (context, target, type) {
+	if (target === \"current\" && !type) {
+		type = target;
+		target = null;
+	}
+
 	if (!target) {
 		if (context.platform.Name === \"twitch\") {
 			target = context.channel.Name;
 		}
 		else {
 			return {
+				success: false,
 				reply: \"You must provide a channel, no default is available outside of Twitch!\"
 			};
 		}
@@ -62,6 +68,19 @@ VALUES
 	const data = vod.data[0];
 	const delta = sb.Utils.timeDelta(new sb.Date(data.created_at));
 	const prettyDuration = data.duration.match(/\\d+[hm]/g).join(\", \");
+
+	if (type === \"current\") {
+		if (!liveString) {			
+			return {
+				reply: `Channel is not currently live, no current timestamp supported.`
+			};
+		}
+
+		const stamp = sb.Utils.parseDuration(data.duration, { target: \"sec\" }) - 90;
+		return {
+			reply: `Current VOD timestamp: ${data.url}?t=${(stamp < 0) ? 0 : stamp}s`
+		};
+	}
 
 	return {
 		reply: `${data.title} (length: ${prettyDuration}) - published ${delta} ${data.url}${liveString}`
