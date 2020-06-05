6b27f17c2c458086eba458be227145f33c26fb2c
Supinic <supinic@protonmail.com>
2020-06-05 19:55:03 +0200

    refactored away legacy format

--- commands/cookie.sql
+++ commands/cookie.sql
@@ -23,51 +23,55 @@ VALUES
 		10000,
 		NULL,
 		NULL,
-		'async (extra, check) => {
-	const data = (await sb.Query.getRecordset(rs => rs
-		.select(\"Cookie_Today\")
-		.from(\"chat_data\", \"Extra_User_Data\")
-		.where(\"User_Alias = %n\", extra.user.ID)
-	))[0];
-
+		'(async function cookie (context, check) {
 	if (check === \"check\") {
-		const ready = (data && data.Cookie_Today) ? \"don\'t\" : \"do\";
-		return { reply: \"You \" + ready + \" have a cookie available today.\" };
+		return {
+			success: false,
+			reply: `Use the check command instead!`
+		};
 	}
 	else if (check === \"gift\" || check === \"give\") {
-		const prefix = sb.Config.get(\"COMMAND_PREFIX\");
-		return { reply: \"You are trying to gift a cookie - you should use the \" + prefix + \"gift cookie <user> command :)\" };
-	}
-	
-	if (data && data.Cookie_Today) {
-		const now = new sb.Date();
-		const midnightUTC = new sb.Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + 1));
-		return { 
-			reply: \"You already opened or gifted a fortune cookie today. You can get another one at midnight UTC, which is \" + sb.Utils.timeDelta(midnightUTC)
+		return {
+			success: false,
+			reply: `Use the give command instead!`
 		};
 	}
 
-	const cookieData = (await sb.Query.getRecordset(rs => rs
+	const data = await sb.Query.getRecordset(rs => rs
+		.select(\"Cookie_Today\")
+		.from(\"chat_data\", \"Extra_User_Data\")
+		.where(\"User_Alias = %n\", context.user.ID)
+		.single()
+	);
+
+	if (data?.Cookie_Today) {
+		const date = new sb.Date();
+		date.setHours(0, 0,0 , 0);
+		date.setTimezoneOffset(0);
+		
+		const delta = sb.Utils.timeDelta(date);
+		return {
+			reply: `You already opened or gifted a fortune cookie today. You can get another one at midnight UTC, which is ${delta}.`
+		};
+	}
+
+	const [cookie] = await sb.Query.getRecordset(rs => rs
 		.select(\"Text\")
 		.from(\"data\", \"Fortune_Cookie\")
 		.orderBy(\"RAND() DESC\")
 		.limit(1)
-	))
+	);
 
-	const cookie = cookieData[0];
-	if (!cookieData[0]) {
-		console.warn(\"No cookie found?\", cookieData);
-		return { reply: \"No cookie found? Please report this to @supinic monkaS\" };
-	}
-
-	await extra.transaction.query([
-		\"INSERT INTO chat_data.Extra_User_Data (User_Alias, Cookie_Today)\", 
-		\"VALUES (\" + extra.user.ID + \", 1)\",
+	await context.transaction.query([
+		\"INSERT INTO chat_data.Extra_User_Data (User_Alias, Cookie_Today)\",
+		\"VALUES (\" + context.user.ID + \", 1)\",
 		\"ON DUPLICATE KEY UPDATE Cookie_Today = 1\"
 	].join(\" \"));
 
-	return { reply: cookie.Text };
-}',
+	return { 
+		reply: cookie.Text
+	};
+})',
 		'async (prefix) => {
 	return [
 		\"Fetch a daily fortune cookie and read its wisdom!\",
