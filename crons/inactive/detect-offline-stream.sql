e6b2d4b2476bad117a82e56433dc00f41dbe1774
Supinic <supinic@protonmail.com>
2020-06-09 21:17:05 +0200

    deactivated

--- crons/inactive/detect-offline-stream.sql
+++ crons/inactive/detect-offline-stream.sql
@@ -16,8 +16,6 @@ VALUES
 		'0 * * * * *',
 		'Checks if the channel \"supinic\" is offline, to unset stream variables like \"sr\", \"tts\" and others.',
 		'(async function cron_detectOfflineStream () {
-	return;
-
 	const sr = sb.Config.get(\"SONG_REQUESTS_STATE\");
 	if (sr === \"off\") {
 		return;
@@ -28,9 +26,10 @@ VALUES
 		sb.Config.set(\"SONG_REQUESTS_STATE\", \"off\");
 		sb.Config.set(\"TTS_ENABLED\", false);
 		
-		sb.Master.send(\"Channel has been detected as offline! Disabling song requests and TTS.\", 38);
+		const channelData = sb.Channel.get(\"supinic\", \"twitch\");
+		await channelData.send(\"Channel has been detected as offline! Disabling song requests and TTS.\");
 	}
 })',
 		'Bot',
-		1
+		0
 	)
