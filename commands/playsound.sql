bbd687692d4d9300216b28604b93e49468848b92
Supinic <supinic@protonmail.com>
2020-06-12 01:44:27 +0200

    now handles cooldowns, instead of the LocalRequest module

--- commands/playsound.sql
+++ commands/playsound.sql
@@ -22,52 +22,61 @@ VALUES
 		'Plays a sound on supinic stream, if enabled. Use \"list\" as an argument to see the list of available playsounds.',
 		10000,
 		NULL,
-		NULL,
+		'(() => {
+	this.data.cooldowns = {};	
+
+	return {
+		fetch: (name) => sb.Query.getRecordset(rs => rs
+			.select(\"*\")
+			.from(\"data\", \"Playsound\")
+			.where(\"Name = %s\", name)
+			.limit(1)
+			.single()
+		)
+	};
+})()',
 		'(async function playSound (context, playsound) {
 	if (!sb.Config.get(\"PLAYSOUNDS_ENABLED\")) {
 		return { reply: \"Playsounds are currently disabled!\" };
 	}
-
 	else if (!playsound || playsound === \"list\") {
 		return { reply: \"Currently available playsounds: https://supinic.com/stream/playsound/list\"};
 	}
 
-	const data = await sb.Query.getRecordset(rs => rs
-		.select(\"Filename\")
-		.from(\"data\", \"Playsound\")
-		.where(\"Name = %s\", playsound.toLowerCase())
-		.where(\"Access = %s\", \"Everyone\")
-		.single()
-	);
-
+	const data = await this.staticData.fetch(playsound);
 	if (!data) {
 		return {
 			reply: \"That playsound either doesn\'t exist or is not available!\",
-			cooldown: null
+			cooldown: 2500
 		};
 	}
 
-	let result = null;
+	this.data.cooldowns[data.Name] = this.data.cooldowns[data.Name] ?? 0;
+	
+	const now = sb.Date.now();
+	if (this.data.cooldowns[data.Name] >= now) {
+		const delta = sb.Utils.timeDelta(this.data.cooldowns[data.Name]);
+		return {
+			reply: `The playsound\'s cooldown has not passed yet! Try again in ${delta}.`
+		};
+	}
+
+	let success = null;
 	try {
-		result = await sb.LocalRequest.playAudio(data.Filename);
+		success = await sb.LocalRequest.playAudio(data.Filename);
 	}
 	catch (e) {
 		await sb.Config.set(\"PLAYSOUNDS_ENABLED\", false);
 		return { reply: \"The desktop listener is not currently running, turning off playsounds!\" }
 	}
-	
 
-	if (typeof result === \"number\") {
-		return {
-			reply: \"The playsound\'s cooldown has not passed yet! Try again in \" + sb.Utils.timeDelta(sb.Date.now() + result),
-			cooldown: null
-		};
-	}
-	else {
-		return {
-			reply: \"Your playsound has been played correctly on stream!\"
-		};
-	}
+	this.data.cooldowns[data.Name] = now + data.Cooldown;
+	return {
+		success,
+		reply: (success)
+			? \"Playsound has been played correctly on stream.\"
+			: \"An error occured while playing the playsound!\"
+	};
 })',
 		NULL,
 		'supinic/supibot-sql'
