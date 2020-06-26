eb26dda566b14a565f10c921697f784f04f6b191
Supinic <supinic@protonmail.com>
2020-06-26 21:45:01 +0200

    user name + duration refactor, reduced cooldown

--- commands/predictlines.sql
+++ commands/predictlines.sql
@@ -20,48 +20,63 @@ VALUES
 		'[\"pl\"]',
 		'ping,pipe',
 		'Predicts the amount of lines a given user will have in some amount of time.',
-		60000,
+		10000,
 		NULL,
 		NULL,
-		'(async function predictLines (context, user, ...args) {
-	if (!user) {
-		return { reply: \"No user provided!\", meta: { skipCooldown: true } };
+		'(async function predictLines (context, ...args) {
+	const { time, ranges } = sb.Utils.parseDuration(args.join(\" \"), { returnData: true });
+	if (time === 0 || ranges.length === 0) {
+		return {
+			success: false,
+			reply: \"No time data provided!\"
+		};
 	}
-	else if (args.length === 0) {
-		return { reply: \"No time provided!\", meta: { skipCooldown: true } };
+	else if (time < 0) {
+		return {
+			success: false,
+			reply: \"Can\'t check for past data!\"
+		};
 	}
 
-	const userData = await sb.User.get(user, true);
+	const timeRanges = ranges.flatMap(i => i.string.split(\" \"));
+	const rest = args.filter(i => !timeRanges.includes(i));
+
+	let userData = context.user;
+	if (rest.length > 0) {
+		userData = await sb.User.get(rest[0]);
+	}
+
 	if (!userData) {
 		return { reply: \"Provided user does not exist!\" };
 	}
 
-	const offset = sb.Utils.parseDuration(args.join(\" \"), { returnData: false });
-	if (!offset || offset < 0) {
+	if (!time || time < 0) {
 		return { reply: \"Provided time must be valid and it must refer to future!\" };
 	}
 
 	const total = (await sb.Query.getRecordset(rs => rs
 		.select(\"SUM(Message_Count) AS Total\")
 		.from(\"chat_data\", \"Message_Meta_User_Alias\")
-		.where(\"User_Alias = %n\", context.user.ID)
+		.where(\"User_Alias = %n\", userData.ID)
 		.single()
 	)).Total;
 
 	const ratio = total / (sb.Date.now() - userData.Started_Using);
-	const messages = sb.Utils.round(offset * ratio, 0);
-
+	const messages = sb.Utils.round(time * ratio, 0);
 	if (messages > Number.MAX_SAFE_INTEGER) {
-		return { reply: \"There\'s too many lines to be precise! Try a smaller time interval.\" };
+		return {
+			reply: \"There\'s too many lines to be precise! Try a smaller time interval.\"
+		};
 	}
 
-	const when = new sb.Date().addMilliseconds(offset);
+	const who = (userData === context.user) ? \"you\" : \"they\";
+	const when = new sb.Date().addMilliseconds(time);
 	const whenString = (when.valueOf())
 		? sb.Utils.timeDelta(when)
 		: \"too far in the future WAYTOODANK\";
 
 	return {
-		reply: \"I predict that there will be \" + messages + \" extra lines \" + whenString
+		reply: `I predict that ${who} will have sent ${messages} messages ${whenString}, on average.`
 	};
 })',
 		NULL,
