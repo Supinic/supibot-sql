ae4d4193609d1bab470f201b69d7cdb2756e5cdc
Supinic <supinic@protonmail.com>
2020-07-07 18:08:28 +0200

    added sub-command for song request stats

--- commands/statistics.sql
+++ commands/statistics.sql
@@ -33,7 +33,7 @@ VALUES
 						.from(\"chat_data\", \"AFK\")
 						.where(\"User_Alias = %n\", context.user.ID)
 						.single();
-	
+
 					if (type === \"total-afk\") {
 						// Do not add a condition - counts totals
 					}
@@ -43,10 +43,10 @@ VALUES
 					else {
 						rs.where(\"Status = %s\", type);
 					}
-	
+
 					return rs;
 				});
-	
+
 				const target = (type === \"total-afk\") ? \"(all combined)\" : type;
 
 				if (!data?.Delta) {
@@ -59,6 +59,107 @@ VALUES
 						reply: `You have been AFK with status \"${target}\" for a total of ${sb.Utils.formatTime(data.Delta)}.`
 					};
 				}
+			}
+		},
+		{
+			names: [\"sr\"],
+			description: \"Checks various sr statistics on supinic\'s channel.\",
+			execute: async function execute (context, type, ...args) {
+				let branch = null;
+				let targetUser = null;
+				let videoID = null;
+
+				if (args.length === 0) {
+					branch = \"user\";
+					targetUser = context.user;
+				}
+				else {
+					const [target] = args;
+					const userCheck = await sb.User.get(target);
+					if (userCheck) {
+						branch = \"user\";
+						targetUser = userCheck;
+					}
+					else {
+						branch = \"video\";
+						videoID = target;
+					}
+				}
+
+				if (branch === \"user\") {
+					return await this.helpers.fetchUserStats(targetUser);
+				}
+				else if (branch === \"video\") {
+					return await this.helpers.fetchVideoStats(videoID);
+				}
+			},
+
+			helpers: {
+				fetchUserStats: async function (targetUser) {
+					const requests = await sb.Query.getRecordset(rs => rs
+						.select(\"Link\", \"Length\", \"Start_Time\", \"End_Time\")
+						.from(\"chat_data\", \"Song_Request\")
+						.where(\"User_Alias = %n\", targetUser.ID)
+					);
+
+					if (requests.length === 0) {
+						return {
+							reply: `No requested videos found.`
+						};
+					}
+
+					const counter = {};
+					let totalLength = 0;
+					let mostRequested = null;
+					let currentMax = 0;
+
+					for (const video of requests) {
+						if (typeof counter[video.Link] === \"undefined\") {
+							counter[video.Link] = 0;
+						}
+
+						counter[video.Link]++;
+						totalLength += (video.End_Time ?? video.Length) - (video.Start_Time ?? 0);
+						if (currentMax < counter[video.Link]) {
+							mostRequested = video.Link;
+							currentMax = counter[video.Link];
+						}
+					}
+
+					const total = sb.Utils.timeDelta(sb.Date.now() + totalLength * 1000, true);
+					return {
+						reply: sb.Utils.tag.trim `
+							Videos requested: ${requests.length}, for a total runtime of ${total}.
+							The most requested video is ${mostRequested} - queued ${currentMax} times.
+						`
+					};
+				},
+				fetchVideoStats: async function (videoID) {
+					if (sb.Utils.linkParser.autoRecognize(videoID)) {
+						videoID = sb.Utils.linkParser.parseLink(videoID);
+					}
+
+					const requests = await sb.Query.getRecordset(rs => rs
+						.select(\"Added\")
+						.from(\"chat_data\", \"Song_Request\")
+						.where(\"Link = %s\", videoID)
+						.orderBy(\"ID DESC\")
+					);
+
+					if (requests.length === 0) {
+						return {
+							reply: `No videos found by given ID.`
+						};
+					}
+
+					const lastDelta = sb.Utils.timeDelta(requests[0].Added);
+					return {
+						reply: sb.Utils.tag.trim `
+							This video has been requested ${requests.length} times.
+							It was last requested ${lastDelta}.
+						`
+					};
+				},
 			}
 		}
 	]
