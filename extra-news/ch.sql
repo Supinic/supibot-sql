6f70e1d2614f7dce5c68ea2b19dc0befe331e772
Supinic <supinic@protonmail.com>
2020-07-14 21:06:56 +0200

    added swiss news

---
+++ extra-news/ch.sql
@@ -0,0 +1,23 @@
+INSERT INTO
+	`data`.`Extra_News`
+	(
+		ID,
+		Code,
+		Language,
+		URL,
+		Endpoints,
+		Type
+	)
+VALUES
+	(
+		17,
+		'ch',
+		'german',
+		'https://www.nzz.ch/',
+		'[
+	\"recent.rss\",
+	\"international.rss\",
+	\"schweiz.rss\"
+]',
+		'RSS'
+	)
