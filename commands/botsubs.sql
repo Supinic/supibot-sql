f712dbd593f3c3cd5985a3794e0c7c76b1b41f01
Supinic <supinic@protonmail.com>
2020-08-12 18:10:42 +0200

    emote data is now handled by Twitch controller
    no need to cache anything within the command

--- commands/botsubs.sql
+++ commands/botsubs.sql
@@ -24,48 +24,22 @@ VALUES
 		NULL,
 		NULL,
 		'(async function botSubs () {
-	const controller = sb.Platform.get(\"twitch\").controller;
-	const emoteSets = controller.availableEmoteSets;
-
-	if (!this.data.emoteSets || emoteSets.join(\",\") !== this.data.emoteSets.join(\",\")) {
-		const { body, statusCode } = await sb.Got({
-			throwHttpErrors: false,
-			url: \"https://api.twitchemotes.com/api/v4/sets\",
-			searchParams: \"id=\" + emoteSets.join(\",\"),
-			responseType: \"json\"
-		});
-
-		if (statusCode !== 200) {
-			throw new sb.errors.APIError({
-				statusCode,
-				apiName: \"TwitchEmotesAPI\"
-			});
-		}
-
-		this.data.fetched = new sb.Date();
-		this.data.emoteSets = body.map(i => i.set_id).sort();
-		this.data.emoteSetsData = body;
-	}
+	const { availableEmotes } = sb.Platform.get(\"twitch\").controller;
+	const subEmoteSets = availableEmotes.filter(i => [\"1\", \"2\", \"3\"].includes(i.tier) && i.emotes.length > 0);
 
 	const result = [];
-	for (const set of controller.availableEmoteSets) {
-		const setData = this.data.emoteSetsData.find(i => i.set_id === set);
-		const emoteList = controller.availableEmotes[set];
-		if (!emoteList || !setData || setData.channel_name === \"Twitch\") {
-			continue;
-		}
-
-		const tier = (setData.tier !== 1) ? ` (T${setData.tier})` : \"\";
+	for (const setData of subEmoteSets) {
+		const tierString = (setData.tier !== \"1\") ? ` (T${setData.tier})` : \"\";
 		result.push({
-			channel: `${setData.channel_name}${tier}`,
-			emote: sb.Utils.randArray(emoteList).code
+			channel: `${setData.channel.login}${tierString}`,
+			emote: sb.Utils.randArray(setData.emotes).token
 		});
 	}
 
 	result.sort((a, b) => a.channel.localeCompare(b.channel));
-
 	const channels = result.map(i => `#${i.channel}`);
 	const emotes = result.map(i => i.emote);
+	
 	return {
 		reply: \"Supibot is currently subbed to: \" + channels.join(\", \") + \" \" + emotes.join(\" \")
 	};
