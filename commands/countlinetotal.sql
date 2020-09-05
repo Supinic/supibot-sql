58e1fd6c78af4b49e60f7daa1db3bfa8f21d14fa
Supinic <supinic@protonmail.com>
2020-09-05 20:01:16 +0200

    added a rate calculation and "death day" calculation

--- commands/countlinetotal.sql
+++ commands/countlinetotal.sql
@@ -47,8 +47,31 @@ VALUES
 		.single()
 	);
 
+	const history = await sb.Query.getRecordset(rs => rs
+		.select(\"Executed\", \"Result\")
+		.from(\"chat_data\", \"Command_Execution\")
+		.where(\"Command = %n\", this.ID)
+		.where(\"Result <> %s\", \"\")
+		.orderBy(\"Executed ASC\")
+		.limit(1)
+		.single()
+	);
+
+	const days = (sb.Date.now() - history.Executed) / 864.0e5;
+	const originalSize = Number(history.Result.match(/([\\d.]+) GB of space/)[1]);
+	const currentSize = sb.Utils.round(data.Bytes / (10 ** 9), 3);
+
+	const rate = sb.Utils.round((currentSize - originalSize) / days, 3);
+	const fillDate = new sb.Date().addDays((220 - currentSize) / rate); // 238 GB minus an estimate of ~18GB of other stuff
+	const megabytesPerHour = sb.Utils.round(rate * 1024 / 24, 3);
+
 	return {
-		reply: \"Currently logging \" + preciseLines + \" lines in total across all channels, taking up approximately \" + sb.Utils.round(data.Bytes / (10 ** 9), 3) + \" GB of space.\"
+		reply: sb.Utils.tag.trim `
+			Currently logging ${preciseLines} lines in total across all channels,
+			taking up ~${currentSize} GB of space.
+			Lines are added at a rate of ~${megabytesPerHour} MB/hr.
+			Supibot\'s hard drive will run out of space approximately on ${fillDate.format(\"Y-m-d\")}.
+		`
 	};
 })',
 		NULL,
